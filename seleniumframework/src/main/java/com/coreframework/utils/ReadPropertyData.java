81af88d147319afd5c1960ec81a6679995b9e697
karim <karim@192.168.0.5>
2020-08-07 20:06:10 -0400

    Committing facebook project with selenium core framework

---
+++ seleniumframework/src/main/java/com/coreframework/utils/ReadPropertyData.java
@@ -0,0 +1,9 @@
+package com.coreframework.utils;
+
+public interface ReadPropertyData {
+	public String readProperty(String key);
+
+	public Boolean isRunningDebug();
+
+	public Boolean isCodeChecksEnabled();
+}
